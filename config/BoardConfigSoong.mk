feb3cdab3b4a024e696ce38ad6ca5b081eef5f3a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-18 09:51:13 +0200

    soong: Drop TARGET_USES_QTI_CAMERA_DEVICE
    * qcom-camera topic hasn't been ported after Android 11
    
    Change-Id: Ifb053d215527d602109bbb451d96ec80cf028548

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -50,8 +50,7 @@ SOONG_CONFIG_derpNvidiaVars += \
 SOONG_CONFIG_NAMESPACES += derpQcomVars
 SOONG_CONFIG_derpQcomVars += \
     supports_extended_compress_format \
-    uses_pre_uplink_features_netmgrd \
-    uses_qti_camera_device
+    uses_pre_uplink_features_netmgrd
 
 # Only create display_headers_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
 ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
@@ -70,7 +69,6 @@ SOONG_CONFIG_derpGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NET
 SOONG_CONFIG_derpNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_derpQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_derpQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
-SOONG_CONFIG_derpQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
