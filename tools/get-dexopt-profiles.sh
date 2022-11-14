fKm86/vendor_benzo

tools: Add script to get dexopt profiles off device
To make it easier to get the profiles for vendor/dexopt_profiles

Change-Id: Ib74763ee44e3720e1b06750a3c43e41a8d9149a7
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

tools/get-dexopt-profiles.sh
@@ -1,0 +1,24 @@
+#!/bin/bash
+
+# Packages to get profiles for
+packages="
+com.android.chrome
+com.android.vending
+com.google.android.apps.messaging
+com.google.android.calculator
+com.google.android.calendar
+com.google.android.contacts
+com.google.android.deskclock
+com.google.android.dialer
+com.google.android.gm
+com.google.android.googlequicksearchbox
+com.google.android.GoogleCamera
+"
+
+# Get the profiles
+for packageName in $packages; do
+    # Get current profile for package
+    adb shell cmd package snapshot-profile $packageName
+    # Pull that profile
+    adb pull /data/misc/profman/$packageName.prof
+done

1cecreamm/vendor_derp

tools/get-dexopt-profiles.sh
@@ -1,0 +1,26 @@
+#!/bin/bash
+
+# Packages to get profiles for
+packages="
+com.android.chrome
+com.android.vending
+com.google.android.apps.messaging
+com.google.android.apps.turbo
+com.google.android.calculator
+com.google.android.calendar
+com.google.android.contacts
+com.google.android.deskclock
+com.google.android.dialer
+com.google.android.gm
+com.google.android.gms
+com.google.android.googlequicksearchbox
+com.google.android.settings.intelligence
+"
+
+# Get the profiles
+for packageName in $packages; do
+    # Get current profile for package
+    adb shell cmd package snapshot-profile $packageName
+    # Pull that profile
+    adb pull /data/misc/profman/$packageName.prof
+done